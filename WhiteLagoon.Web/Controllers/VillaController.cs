4049531c3b8224f74a4b8f636a385a885481d2a6
Rupam Bhoyar <rupambhoyar@gmail.com>
2025-05-29 00:22:54 +0530

    Villa CRUD

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 
 namespace WhiteLagoon.Web.Controllers
@@ -20,6 +21,68 @@ namespace WhiteLagoon.Web.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(obj);
+            }
+            _context.Villas.Add(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(int villaId)
+        {
+            Villa? obj = _context.Villas.FirstOrDefault(i => i.Id == villaId);
+            if (obj is null)
+            {
+                return RedirectToAction("Error","Home");
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (!ModelState.IsValid || obj.Id == 0)
+            {
+                return View(obj);
+            }
+
+            _context.Villas.Update(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _context.Villas.FirstOrDefault(i => i.Id == villaId);
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _context.Villas.FirstOrDefault(i => i.Id == obj.Id);
+
+            if(objFromDb is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            _context.Villas.Remove(objFromDb);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
