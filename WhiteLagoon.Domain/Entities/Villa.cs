d7fce3a17b3f2002385dd9e7d4b25ad474bc8e34
Rupam Bhoyar <rupambhoyar@gmail.com>
2025-06-10 23:55:59 +0530

    Added image upload functionality to villa

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,6 +28,9 @@ namespace WhiteLagoon.Domain.Entities
         [Range(1,10)]
 
         public int Occupancy { get; set; }
+
+        [NotMapped]
+        public IFormFile? Image { get; set; }
 
         [DisplayName("Image Url")]
         public string? ImageUrl { get; set; }
